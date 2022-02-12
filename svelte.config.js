e567b44b9fc2b2f429c6b48b93d805398b5f0358
Alan <john1337@gmail.com>
2022-02-12 17:32:25 -0600

    Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
+const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +10,9 @@ const config = {
       	define: {
         'process.env': process.env,
       },
+	    ssr: {
+                noExternal: dev ? [] : ['@supabase/supabase-js']
+            },
     },
 	},
 
