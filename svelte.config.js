bdf48598187b25bb404d3c4b2d2e47a1a38493dc
Alan <john1337@gmail.com>
2022-02-12 17:16:24 -0600

    Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
-const netlify = require('@sveltejs/adapter-netlify')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-
 		vite: {
       	define: {
         'process.env': process.env,
